bc95ef7ccf5f94280bd55e823d41c3fdf71b81e3
stigkristiansen <stigkristiansen@outlook.com>
2021-10-24 15:28:13 +0200

    Update module.php

--- DayAheadPrices/module.php
+++ DayAheadPrices/module.php
@@ -132,7 +132,7 @@ class DayAheadPrices extends IPSModule {
 
 	public function ReceiveData($JSONString) {
 		$data = json_decode($JSONString);
-		IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+		IPS_LogMessage('Device RECV', $data->Buffer);
 	}
 
 	private function InitTimer() {
