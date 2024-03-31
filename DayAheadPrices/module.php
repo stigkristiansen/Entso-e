84643a31b89fd3e53f7a447b8f4ed8afe6396f5b
stigkristiansen <stigkristiansen@outlook.com>
2024-03-31 17:24:45 +0200

    Update module.php

--- DayAheadPrices/module.php
+++ DayAheadPrices/module.php
@@ -357,6 +357,8 @@ class DayAheadPrices extends IPSModule {
 		$points = $prices->Prices->Points;
 
 		$this->SendDebug(__FUNCTION__, sprintf('Hourly prices: %s', json_encode($points)), 0);
+		$this->SendDebug(__FUNCTION__, sprintf('Hourly price for 23 is: %f', $points[23]), 0);
+
 		
 		$lowestIdx = 0;
 		$lowestSum = PHP_FLOAT_MAX;
